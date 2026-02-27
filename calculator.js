8e1af9fca25425edfee59b22ee67084e63f2cbbc
Satyam Kushwaha <satyamkushwaha2511@gmail.com>
2026-02-27 22:50:29 +0530

    new update

---
+++ calculator.js
@@ -0,0 +1,7 @@
+let promise =new Promise((resolve, reject) => {
+console.log("I am a Promise");
+});
+let promise = new Promise((resolve, reject) => {
+  console.log("I am a Promise");
+  resolve("Success!");
+});
