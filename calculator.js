30992cbed0266e37ebe7319da0fc2f1e8bd1d3c5
Satyam Kushwaha <satyamkushwaha2511@gmail.com>
2026-03-10 16:31:13 +0530

    object

--- calculator.js
+++ calculator.js
@@ -5,3 +5,9 @@ let promise = new Promise((resolve, reject) => {
   console.log("I am a Promise");
   resolve("Success!");
 });
+const cars = ["BMW","volvo", "saab", "Ford","Flat", "Audi"];
+
+console.log(cars);
+for(let i =0; i<cars.length;i++){
+console.log(cars[i]);
+}
